ea7afbc637bb76103491460bd348b547630f9484
CYF <604493621@qq.com>
2024-03-05 23:30:26 +0800

    layout-渲染一级导航列表

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,15 @@
 </script>
 
 <template>
-    <el-button type="primary">Primary</el-button>
-  
+   <!-- 一级路由出口组件 -->
+   <RouterView></RouterView>
+      test scss
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.test{
+  color:$priceColor;
+}
 header {
   line-height: 1.5;
   max-height: 100vh;
